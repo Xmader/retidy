ed7505ab96170ca6219f03cfd4d572e3e31649e0
Xmader <xmader@outlook.com>
2019-11-20 18:38:12 -0500

    UnminifyBooleans

---
+++ src/unminify-booleans.ts
@@ -0,0 +1,25 @@
+
+import VisitorWrapper from "./utils/visitor-wrapper"
+import { NumericLiteral, booleanLiteral, isNumericLiteral } from "@babel/types"
+
+export const UnminifyBooleans = VisitorWrapper({
+    UnaryExpression(path) {
+        const { node } = path
+
+        if (node.operator == "!" && isNumericLiteral(node.argument)) {
+            const n = node.argument as NumericLiteral
+
+            switch (n.value) {
+                case 0:
+                    path.replaceWith(booleanLiteral(true))
+                    break
+
+                case 1:
+                    path.replaceWith(booleanLiteral(false))
+                    break
+            }
+        }
+    },
+})
+
+export default UnminifyBooleans
