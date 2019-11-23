b5bb7688d24ca49a8a5d5849a9a46a6da0c531a0
Xmader <xmader@outlook.com>
2019-11-23 16:09:14 -0500

    generator defaultOptions

--- src/generator/generator.ts
+++ src/generator/generator.ts
@@ -3,6 +3,21 @@ import { Printer } from "./printer"
 import { Node } from "@babel/types"
 import { Options } from "recast/lib/options"
 
+const defaultOptions = {
+    lineTerminator: "\n",
+    quote: "double",
+    trailingComma: {
+        objects: true,
+        arrays: false,
+        parameters: false,
+    },
+}
+
 export const prettyPrint = (ast: Node, options?: Options) => {
+    options = Object.assign({}, defaultOptions, options || {})
     return new Printer(options).printGenerically(ast)
 }
+
+/** @alias prettyPrint */
+export const generator = prettyPrint
+export default generator
