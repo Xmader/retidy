7f2414eec8b9247532ecb46a49ecc3eb51baeb9e
Xmader <xmader@outlook.com>
2019-11-24 13:21:13 -0500

    ast utils

--- src/utils/ast.ts
+++ src/utils/ast.ts
@@ -1,4 +1,22 @@
 
-import { Node } from "@babel/types"
+import { Node, isNode } from "@babel/types"
 
 export type AST = Node | Node[]
+
+export const isAST = (ast: AST) => {
+    if (Array.isArray(ast)) {
+        return ast.every(isNode)
+    } else {
+        return isNode(ast)
+    }
+}
+
+export const assertNotASTArray = (ast: AST) => {
+    if (!isAST(ast)) {
+        throw new TypeError("ast is not an instance of AST.")
+    }
+
+    if (Array.isArray(ast)) {
+        throw new TypeError("not support for AST array input.")
+    }
+}
