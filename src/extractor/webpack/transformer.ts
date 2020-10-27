19ab75bba510bd3e33a388e3d84f5864d0d82b61
Xmader <xmader@outlook.com>
2020-10-27 01:28:39 -0400

    feat: webpack runtime function conversion

--- src/extractor/webpack/transformer.ts
+++ src/extractor/webpack/transformer.ts
@@ -4,16 +4,30 @@ import { ModuleId } from "../module"
 import {
     isVariableDeclaration,
     isIdentifier,
+    isMemberExpression,
     isNumericLiteral,
     isStringLiteral,
+    isFunctionExpression,
     identifier,
     callExpression,
     stringLiteral,
+    memberExpression,
+    objectExpression,
+    objectProperty,
+    booleanLiteral,
+    Identifier,
+    FunctionExpression,
 } from "@babel/types"
 
 const transformErr = () => new Error("something goes wrong.")
+const unknownRuntimeFnErr = () => new Error("unknown webpack runtime function")
 
 const REQUIRE_CALLEE = identifier("require")
+const DEFINEPROPERTY_CALLEE = memberExpression(identifier("Object"), identifier("defineProperty")) // Object.defineProperty
+
+const isWebpackRequire = (node: object): node is Identifier => {
+    return isIdentifier(node, { name: "__webpack_require__" })
+}
 
 /**
  * Transform variables defined in ModuleFunction params to their real values
@@ -51,28 +65,74 @@ export const getModuleFunctionParamsTransformer = (entryId: ModuleId) => {
             node.body.shift()
         },
 
-        // transform `__webpack_require__` to normal `require`
-        // `__webpack_require__(0)` -> `require("./0")`
+        // transform `__webpack_require__`
         CallExpression(path) {
             const { node } = path
-            const { callee, arguments: { 0: requireIdE } } = node
+            const { callee, arguments: callArgs } = node
 
             // check __webpack_require__
-            if (!isIdentifier(callee, { name: "__webpack_require__" })) {
-                return
+            if (isWebpackRequire(callee)) {
+                // transform `__webpack_require__` to normal `require`
+                // `__webpack_require__(0)` -> `require("./0")`
+
+                const { 0: requireIdE } = callArgs
+                if (!isNumericLiteral(requireIdE) && !isStringLiteral(requireIdE)) {
+                    throw transformErr()
+                }
+
+                const requireId = requireIdE.value
+                const isEntryRequire = requireId == entryId
+                const requirePath = `./${isEntryRequire ? "entry_" : ""}${requireId}`
+
+                path.replaceWith(
+                    callExpression(REQUIRE_CALLEE, [stringLiteral(requirePath)])
+                )
+            } else if ( // __webpack_require__.fn(exports, …
+                isMemberExpression(callee) &&
+                isWebpackRequire(callee.object) &&
+                isIdentifier(callee.property) &&
+                isIdentifier(callArgs[0], { name: "exports" })
+            ) {
+                if (callee.property.name == "d") {  // callArgs.length > 1
+                    // transform `__webpack_require__.d(exports, "…", function(){…})`
+                    // __webpack_require__.d: define getter function for harmony exports
+                    const [, exportName, exportGetter] = callArgs
+                    if (!isStringLiteral(exportName) && !isFunctionExpression(exportGetter)) {
+                        console.log(node)
+                        throw transformErr()
+                    }
+
+                    path.replaceWith(
+                        callExpression(DEFINEPROPERTY_CALLEE, [ // Object.defineProperty(exports, name, {…
+                            identifier("exports"),
+                            exportName,
+                            objectExpression([ // { enumerable: true, get: exportGetter }
+                                objectProperty(identifier("enumerable"), booleanLiteral(true)),
+                                objectProperty(identifier("get"), exportGetter as FunctionExpression),
+                            ])
+                        ])
+                    )
+                } else if (callee.property.name == "r") {
+                    // transform `__webpack_require__.r(exports)
+                    // __webpack_require__.r: define __esModule on exports
+                    //                        (useless for our "re-tidied" code)
+
+                    // path.replaceWithSourceString(
+                    //      `Object.defineProperty(exports, '__esModule', { value: true });`
+                    // )
+                    path.replaceWith(
+                        callExpression(DEFINEPROPERTY_CALLEE, [
+                            identifier("exports"),
+                            stringLiteral("__esModule"),
+                            objectExpression([
+                                objectProperty(identifier("value"), booleanLiteral(true)),
+                            ])
+                        ])
+                    )
+                } else {
+                    throw unknownRuntimeFnErr()
+                }
             }
-
-            if (!isNumericLiteral(requireIdE) && !isStringLiteral(requireIdE)) {
-                throw transformErr()
-            }
-
-            const requireId = requireIdE.value
-            const isEntryRequire = requireId == entryId
-            const requirePath = `./${isEntryRequire ? "entry_" : ""}${requireId}`
-
-            path.replaceWith(
-                callExpression(REQUIRE_CALLEE, [stringLiteral(requirePath)])
-            )
         },
 
     })
